4600f3cb6ea824f22c2101acfd4976e876041ec5
김용만 <kym@gim-yongmans-MacBook-Pro.local>
2021-04-05 16:02:46 +0900

    kym

---
+++ dockerfile
@@ -0,0 +1,25 @@
+FROM node:12.16.2 as builder
+
+# 작업 폴더를 만들고 npm 설치
+RUN mkdir /usr/src/app
+WORKDIR /usr/src/app
+ENV PATH /usr/src/app/node_modules/.bin:$PATH
+COPY package.json /usr/src/app/package.json
+RUN npm install --silent
+RUN npm install react-scripts@3.4.1 -g --silent
+
+# 소스를 작업폴더로 복사하고 빌드
+COPY . /usr/src/app
+RUN npm run build
+
+FROM nginx:latest
+# nginx의 기본 설정을 삭제하고 앱에서 설정한 파일을 복사
+RUN rm -rf /etc/nginx/conf.d
+COPY conf /etc/nginx
+
+# 위에서 생성한 앱의 빌드산출물을 nginx의 샘플 앱이 사용하던 폴더로 이동
+COPY --from=builder /usr/src/app/build /usr/share/nginx/html
+
+# 80포트 오픈하고 nginx 실행
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
---
+++ src/services/EmployeeService.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
+class EmployeeService {
+  getEmployees() {
+    return axios.get(EMPLOYEE_API_BASE_URL);
+  }
+  addEmployee(employee) {
+    return axios.post(EMPLOYEE_API_BASE_URL, employee);
+  }
+  getEmployeeById(id) {
+    return axios.get(EMPLOYEE_API_BASE_URL + "/" + id);
+  }
+  updateEmployee(employee) {
+    return axios.put(EMPLOYEE_API_BASE_URL, employee);
+  }
+  deleteEmployee(employee) {
+    return axios.post(EMPLOYEE_API_BASE_URL + "/delete", employee);
+  }
+  deleteById(id) {
+    return axios.delete(EMPLOYEE_API_BASE_URL + "/" + id);
+  }
+}
+
+export default new EmployeeService();
