b12ce63f2364af552e61b957ec296806652cd409
kym <kym>
2021-04-07 05:36:13 +0000

    kym

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "http://emp.kimdragon50.ml/api/v1/employees";
 class EmployeeService {
   getEmployees() {
     return axios.get(EMPLOYEE_API_BASE_URL);
