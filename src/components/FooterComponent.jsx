89754016db949c6b2e2a416b65a5f0df7b69fcdb
kimdragon <53426549+kimdragon50@users.noreply.github.com>
2022-11-21 08:21:39 +0900

    Update FooterComponent.jsx

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -6,7 +6,7 @@ export default class FooterComponent extends Component {
       <div>
         <footer className="footer">
           <span className="text-muted">
-            All Rights Reserved 2020 @DursunEryilmaz
+
           </span>
         </footer>
       </div>
