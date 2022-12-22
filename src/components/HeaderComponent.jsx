1399194fe84d600a6aaf3b5a3efda396b046a830
kimdragon50 <kimdragon50@gmail.com>
2022-12-23 07:53:16 +0900

    text reset

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -12,8 +12,8 @@ export default class HeaderComponent extends Component {
         <header>
           <nav className="navbar navbar-expand-md navbar-dark bg-dark">
             <div>
-              <a href="http://ecs.fastcampus-dragon.net" className="navbar-brand">
-                cicd_test
+              <a href="http://www.fastcampus-dragon.net" className="navbar-brand">
+                kimdragon
               </a>
             </div>
           </nav>
