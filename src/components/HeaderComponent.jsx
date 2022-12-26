13de5872a4ff8bee53902cdea3dd53465a4738d5
kimdragon50 <kimdragon50@gmail.com>
2022-12-26 11:15:40 +0900

    change Header text

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -12,8 +12,8 @@ export default class HeaderComponent extends Component {
         <header>
           <nav className="navbar navbar-expand-md navbar-dark bg-dark">
             <div>
-              <a href="http://www.fastcampus-dragon.net" className="navbar-brand">
-                kimdragon
+              <a href="http://ecs.fastcampus-dragon.net" className="navbar-brand">
+                ecs code pipe line test!!!
               </a>
             </div>
           </nav>
